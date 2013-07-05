5f2da1f2a31411ff7704a8afda0e3d9c08e9426d
Carl Jahn <jahncarl@gmail.com>
2013-07-05 14:41:13 +0200

    - replaced Headerinformations

--- libFIOApi/libFIOApi/classes/FIORequestBlockOperation.m
+++ libFIOApi/libFIOApi/classes/FIORequestBlockOperation.m
@@ -3,7 +3,7 @@
 //  libFIOApi
 //
 //  Created by carl.jahn on 27.03.13.
-//  Copyright (c) 2013 Carl Jahn (carl.jahn@nidag.com), NIDAG GmbH. All rights reserved.
+//  Copyright (c) 2013 Carl Jahn (https://github.com/carlj). All rights reserved.
 //
 
 #import "FIORequestBlockOperation.h"
